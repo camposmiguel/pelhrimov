08bc7e3f309eff54ffa89621e8a00ceadd2d7cf4
Miguel Campos <camposmiguel@gmail.com>
2017-04-03 15:48:26 +0200

    TicTacToe

--- 05_Games/01_TicTacToe/app/src/main/java/com/miguelcr/a01_tictactoe/GameActivity.java
+++ 05_Games/01_TicTacToe/app/src/main/java/com/miguelcr/a01_tictactoe/GameActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActivity extends AppCompatActivity {
     TextView player;
@@ -16,6 +17,27 @@ public class GameActivity extends AppCompatActivity {
     imageViewCell7,imageViewCell8,imageViewCell9;
     boolean isPlayingPlayer1 = true;
     String name1, name2;
+    /*
+    |  0  |  1 |  2 |
+    -----------------
+    |  3  |  4 |  5 |
+    -----------------
+    |  6  |  7 |  8 |
+    -----------------
+
+     |  0  |  0 |  0 |
+    -----------------
+    |  0  |  0 |  0 |
+    -----------------
+    |  0  |  0 |  0 |
+    -----------------
+    */
+
+    //                     0 1 2 3 4 5 6 7 8
+    int[] selectedCells = {0,0,0,0,0,0,0,0,0};
+
+    // if Player1 select one cell > 1
+    // if Player2 select one cell > 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,49 +79,118 @@ public class GameActivity extends AppCompatActivity {
     public void cellClicked(View v) {
         int id = v.getId();
 
-        int icon = R.drawable.ic_player1;
-        if(!isPlayingPlayer1) {
-            icon = R.drawable.ic_player2;
-        }
+        int position = 0;
+        ImageView imageViewCurrent = null;
 
         switch (id) {
             case R.id.cell1:
-                imageViewCell1.setImageResource(icon);
+                imageViewCurrent = imageViewCell1;
+                position = 0;
                 break;
             case R.id.cell2:
-                imageViewCell2.setImageResource(icon);
+                imageViewCurrent = imageViewCell2;
+                position = 1;
             case R.id.cell3:
-                imageViewCell3.setImageResource(icon);
+                imageViewCurrent = imageViewCell3;
+                position = 2;
                 break;
             case R.id.cell4:
-                imageViewCell4.setImageResource(icon);
+                imageViewCurrent = imageViewCell4;
+                position = 3;
                 break;
             case R.id.cell5:
-                imageViewCell5.setImageResource(icon);
+                imageViewCurrent = imageViewCell5;
+                position = 4;
                 break;
             case R.id.cell6:
-                imageViewCell6.setImageResource(icon);
+                imageViewCurrent = imageViewCell6;
+                position = 5;
                 break;
             case R.id.cell7:
-                imageViewCell7.setImageResource(icon);
+                imageViewCurrent = imageViewCell7;
+                position = 6;
                 break;
             case R.id.cell8:
-                imageViewCell8.setImageResource(icon);
+                imageViewCurrent = imageViewCell8;
+                position = 7;
                 break;
             case R.id.cell9:
-                imageViewCell9.setImageResource(icon);
+                imageViewCurrent = imageViewCell9;
+                position = 8;
                 break;
         }
 
-        if(isPlayingPlayer1) {
-            player.setText(name2);
-            isPlayingPlayer1 = false;
+        if(selectedCells[position]==0) {
+            if(isPlayingPlayer1) {
+                player.setText(name2);
+                imageViewCurrent.setImageResource(R.drawable.ic_player1);
+                selectedCells[position] = 1;
+
+                if(checkSolution()) {
+                    Toast.makeText(this, "Player 1 wins!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isPlayingPlayer1 = false;
+                }
+            } else {
+                player.setText(name1);
+                imageViewCurrent.setImageResource(R.drawable.ic_player2);
+                selectedCells[position] = 2;
+
+                if(checkSolution()) {
+                    Toast.makeText(this, "Player 2 wins!", Toast.LENGTH_SHORT).show();
+                } else {
+                    isPlayingPlayer1 = true;
+                }
+            }
+
+
         } else {
-            player.setText(name1);
-            isPlayingPlayer1 = true;
+            Toast.makeText(this, "The selected cell is not empty", Toast.LENGTH_SHORT).show();
         }
 
 
+    }
+
+    public boolean checkSolution() {
+        boolean existSolution = false;
+
+
+        if(selectedCells[0]==selectedCells[1] &&
+                selectedCells[1]==selectedCells[2]) {
+            // 0, 1, 2 > 1st row
+            existSolution = true;
+        } else if(selectedCells[3]==selectedCells[4] &&
+                selectedCells[4]==selectedCells[5]) {
+            // 3, 4, 5, > 2nd row
+            existSolution = true;
+        } else if(selectedCells[6]==selectedCells[7] &&
+                selectedCells[7]==selectedCells[8]) {
+            // 6,7,8 > 3rd row
+            existSolution = true;
+        } else if(selectedCells[0]==selectedCells[3] &&
+                selectedCells[3]==selectedCells[6]) {
+            // 0,3,6 > 1st col
+            existSolution = true;
+        } else if(selectedCells[1]==selectedCells[4] &&
+                selectedCells[4]==selectedCells[7]) {
+            // 1,4,7 > 2nd col
+            existSolution = true;
+        } else if(selectedCells[2]==selectedCells[5] &&
+                selectedCells[5]==selectedCells[8]) {
+            // 2,5,8 > 3rd col
+            existSolution = true;
+        } else if(selectedCells[0]==selectedCells[4] &&
+                selectedCells[4]==selectedCells[8]) {
+            // 0,4,8 > left diagonal
+            existSolution = true;
+        } else if(selectedCells[2]==selectedCells[4] &&
+                selectedCells[4]==selectedCells[6]) {
+            // 2,4,6 > right diagonal
+            existSolution = true;
+        }
+
+
+        return existSolution;
     }
 
 }
