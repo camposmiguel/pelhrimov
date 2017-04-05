17aeb6524fa748e7c0c384e43afc0e46f1b5851b
Miguel Campos <camposmiguel@gmail.com>
2017-04-05 19:06:02 +0200

    Chat

--- 07_AppServer/01_ChatApp/app/src/main/java/com/miguelcr/a01_chatapp/MainActivity.java
+++ 07_AppServer/01_ChatApp/app/src/main/java/com/miguelcr/a01_chatapp/MainActivity.java
@@ -41,12 +41,33 @@ public class MainActivity extends AppCompatActivity {
 
         listViewMessages = (ListView) findViewById(R.id.listView);
 
+        getDataFromServer();
 
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_refresh) {
+            getDataFromServer();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void getDataFromServer() {
         // Get data from server
         // Connection configuration
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://miguelftp.esy.es/")
+                .baseUrl("http://ferdaferdinand.esy.es/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
@@ -77,31 +98,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
